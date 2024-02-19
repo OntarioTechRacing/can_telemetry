0c445fe84d1320dc76dbba931502e9858acfd520
Daniel Jeon <48095779+danielljeon@users.noreply.github.com>
2024-02-19 18:17:54 -0500

    Add app.py with logging capabilities

--- main.py
+++ main.py
@@ -1,22 +1,30 @@
-if __name__ == "__main__":
-    # TODO: TEST CODE for #1.
-    # import time
-    #
-    # from bus_manager_class import CANBusManager
-    #
-    # def print_message(msg):
-    #     print(f"Received message: {msg}")
-    #
-    # filters = [{"can_id": 0x123, "can_mask": 0x7FF, "extended": False}]
-    # manager = CANBusManager(filters=filters)
-    #
-    # manager.add_listener(print_message)
-    # manager.start()
-    #
-    # try:
-    #     while True:
-    #         time.sleep(1)
-    # except KeyboardInterrupt:
-    #     manager.stop()
+"""Main application module."""
 
+if __name__ == "__main__":
+    # TODO: DEV TEST CODE.
+    # import time
+    # from can import Message
+    # from app import CANTelemetryApp, CANInterface
+    #
+    # current_time = time.time()
+    # message_list = [
+    #     Message(
+    #         arbitration_id=0x001,
+    #         data=b"\x01\x02\x03\x04",
+    #         timestamp=current_time + 2,
+    #     ),
+    #     Message(
+    #         arbitration_id=0x002,
+    #         data=b"\x05\x06\x07\x08",
+    #         timestamp=current_time + 4,
+    #     ),
+    # ]
+    #
+    # app = CANTelemetryApp(
+    #     interface=CANInterface.SIM,
+    #     sim_messages=message_list,
+    #     csv_logging=True,
+    #     ascii_logging=True,
+    # )
+    # app.start()
     pass

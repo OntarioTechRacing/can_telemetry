bdc91ff232f84771eb380f365e51f77bc5217487
Daniel Jeon <48095779+danielljeon@users.noreply.github.com>
2024-02-19 12:27:19 -0500

    Add placeholder main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+if __name__ == "__main__":
+    # TODO: TEST CODE for #1.
+    # import time
+    #
+    # from bus_manager_class import CANBusManager
+    #
+    # def print_message(msg):
+    #     print(f"Received message: {msg}")
+    #
+    # filters = [{"can_id": 0x123, "can_mask": 0x7FF, "extended": False}]
+    # manager = CANBusManager(filters=filters)
+    #
+    # manager.add_listener(print_message)
+    # manager.start()
+    #
+    # try:
+    #     while True:
+    #         time.sleep(1)
+    # except KeyboardInterrupt:
+    #     manager.stop()
+
+    pass

41f2370ba68149d42cd950f01c03a250d5f1c791
Cognizaunce <80372536+Cognizaunce@users.noreply.github.com>
2024-05-25 19:53:59 -0400

    Added more documentation, fixed broken code accidentally pushed (mostly changed references to start.json back to config.json)

--- main.py
+++ main.py
@@ -26,27 +26,6 @@ if __name__ == "__main__":
         print(f"An IOError occurred: {e}")
 
     # Load JSON data from a file
-    with content:
-        data = json.load(content)
-
-    start = "start.json"
-
-    # Open the file and read its contents
-    try:
-        with open(start, 'r') as file:
-            content = file.read()
-            if not content:
-                raise ValueError(f"The file {start} is empty.")
-            else:
-                print("Content:", content)
-    except FileNotFoundError:
-        print(f"The file {start} does not exist.")
-        exit()  # Exit the program if the file does not exist
-    except IOError as e:
-        print(f"An IOError occurred: {e}")
-        exit()  # Exit the program if an IOError occurs
-
-    # Load JSON data from a file
     try:
         app_list = json.loads(content)
     except json.JSONDecodeError as e:
@@ -56,23 +35,25 @@ if __name__ == "__main__":
     # Now `data` contains the JSON content
     print("Data:", app_list)
 
+    app_dir: str = ""
+    keep_running: bool = True
+    while keep_running:
+        # Load config.
+        config = CANTelemetryAppConfig.init_from_dir(app_dir)
 
-    # Load config.
-    config = CANTelemetryAppConfig.init_from_dir(app_dir)
+        # Init telemetry backend app.
+        telemetry = config.init_app()
+        telemetry.start()
 
-    # Init telemetry backend app.
-    telemetry = config.init_app()
-    telemetry.start()
+        # Init and run GUI.
+        gui = QtWidgets.QApplication(sys.argv)
+        mainWindow = MainWindow(config.gui_ui)
+        mainWindow.show()
 
-    # Init and run GUI.
-    gui = QtWidgets.QApplication(sys.argv)
-    mainWindow = MainWindow(config.gui_ui)
-    mainWindow.show()
-
-    try:
-        gui.exec()
-    except KeyboardInterrupt:  # GUI closed.
-        pass
-    finally:
-        telemetry.stop()
-        telemetry.join()
+        try:
+            gui.exec()
+        except KeyboardInterrupt:  # GUI closed.
+            pass
+        finally:
+            telemetry.stop()
+            telemetry.join()
